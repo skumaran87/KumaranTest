8266f2ff30da6e2b8c922bca64f56b28c29dfb26
skumaran87 <30585538+skumaran87@users.noreply.github.com>
2017-10-23 11:24:26 +1100

    update File

--- SpringRestService/src/test/java/com/silverrailtech/SpringRestService/SpringRestServiceApplicationTests.java
+++ SpringRestService/src/test/java/com/silverrailtech/SpringRestService/SpringRestServiceApplicationTests.java
@@ -1,16 +1,80 @@
-package com.silverrailtech.SpringRestService;
+package com.silverrailtech.SpringRestService.test;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.MediaType;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
-@RunWith(SpringRunner.class)
+import com.silverrailtech.SpringRestService.SpringRestServiceApplication;
+import com.silverrailtech.SpringRestService.client.SecureController;	
+
+@ContextConfiguration(classes = SpringRestServiceApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-public class SpringRestServiceApplicationTests {
+public class TestStringStateAPI {
 
-	@Test
-	public void contextLoads() {
+	public TestStringStateAPI() {
+		// TODO Auto-generated constructor stub
 	}
+	
+
+
+	private MockMvc mock;
+	
+	
+	private ApplicationContext context;
+	
+		
+	 @InjectMocks
+	    private SecureController secureController;
+	 
+	@Before
+	public void setup() {
+		context= new ClassPathXmlApplicationContext("spring.xml");
+       this.mock = MockMvcBuilders.standaloneSetup(context).build();
+    		   //.webAppContextSetup(context).build();
+
+	}
+	
+	@Test
+    public void launhTestWebApp() throws Exception{
+       
+       //this.mock = MockMvcBuilders.webAppContextSetup(this.context).build();
+        
+    }
+
+	
+	@Test
+	public void verifyInvalidPO() throws Exception {
+		String character = "a";
+		String amount = "2";
+		mock.perform(MockMvcRequestBuilders.post("/test/char")//.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON).content("{\"character\":\""+character+"\",\"amount\":\""+amount+"\"}")
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.entity").exists())
+		.andDo(print());
+		
+	
+	}
+	
+
+
 
 }
